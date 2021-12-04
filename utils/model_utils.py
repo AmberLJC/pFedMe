955bcff13e437fede8d2abb7b11774caeb72fbc5
AmberLJC <amberljc@umich.edu>
2021-12-04 12:20:56 -0500

    mnist

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -78,7 +78,7 @@ def get_batch_sample(data, batch_size):
 
 def read_cifa_data():
     transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
+    
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=len(trainset.data),shuffle=False)
@@ -254,38 +254,8 @@ def read_data(dataset):
         train_data: dictionary of train data
         test_data: dictionary of test data
     '''
-
-    if(dataset == "Cifar10"):
-        clients, groups, train_data, test_data = read_cifa_data()
-        return clients, groups, train_data, test_data
-
-    train_data_dir = os.path.join('data',dataset,'data', 'train')
-    test_data_dir = os.path.join('data',dataset,'data', 'test')
-    clients = []
-    groups = []
-    train_data = {}
-    test_data = {}
-
-    train_files = os.listdir(train_data_dir)
-    train_files = [f for f in train_files if f.endswith('.json')]
-    for f in train_files:
-        file_path = os.path.join(train_data_dir, f)
-        with open(file_path, 'r') as inf:
-            cdata = json.load(inf)
-        clients.extend(cdata['users'])
-        if 'hierarchies' in cdata:
-            groups.extend(cdata['hierarchies'])
-        train_data.update(cdata['user_data'])
-
-    test_files = os.listdir(test_data_dir)
-    test_files = [f for f in test_files if f.endswith('.json')]
-    for f in test_files:
-        file_path = os.path.join(test_data_dir, f)
-        with open(file_path, 'r') as inf:
-            cdata = json.load(inf)
-        test_data.update(cdata['user_data'])
-
-    clients = list(sorted(train_data.keys()))
+     
+    clients, groups, train_data, test_data = read_dataset (dataset)
 
     return clients, groups, train_data, test_data
 
@@ -355,3 +325,134 @@ class Metrics(object):
             os.mkdir(os.path.join('out', self.params['dataset']))
         with open(metrics_dir, 'w') as ouf:
             json.dump(metrics, ouf)
+
+
+
+def read_dataset(data = 'Cifar10'):
+    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    if data == 'Cifar10':
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
+
+        # Setup directory for train/test data
+        train_path = './data/train/cifa_train_100.json'
+        test_path = './data/test/cifa_test_100.json'
+        num_class =  10
+        
+    elif data == 'EMNIST' :
+        trainset = torchvision.datasets.EMNIST(root='./data',split="byclass", train=True,download=True, transform=transform)
+        testset = torchvision.datasets.EMNIST(root='./data', split="byclass", train=False,download=True, transform=transform)
+        train_path = './data/train/cifa_train_100.json'
+        test_path = './data/test/cifa_test_100.json'
+        num_class  =62
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=len(trainset.data),shuffle=False)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=len(testset.data),shuffle=False)
+
+    for _, train_data in enumerate(trainloader,0):
+        trainset.data, trainset.targets = train_data
+    for _, train_data in enumerate(testloader,0):
+        testset.data, testset.targets = train_data
+
+    NUM_USERS = 20 # should be muitiple of 10
+    NUM_LABELS = 3
+    random.seed(1)
+    np.random.seed(1)
+    dir_path = os.path.dirname(train_path)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    dir_path = os.path.dirname(test_path)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    cifa_data_image = []
+    cifa_data_label = []
+
+    cifa_data_image.extend(trainset.data.cpu().detach().numpy())
+    cifa_data_image.extend(testset.data.cpu().detach().numpy())
+    cifa_data_label.extend(trainset.targets.cpu().detach().numpy())
+    cifa_data_label.extend(testset.targets.cpu().detach().numpy())
+    cifa_data_image = np.array(cifa_data_image)
+    cifa_data_label = np.array(cifa_data_label)
+
+    cifa_data = []
+    for i in trange(num_class):
+        idx = cifa_data_label==i
+        cifa_data.append(cifa_data_image[idx])
+
+
+    print("\nNumb samples of each label:\n", [len(v) for v in cifa_data])
+    users_lables = []
+
+    ###### CREATE USER DATA SPLIT #######
+    # Assign 100 samples to each user
+    X = [[] for _ in range(NUM_USERS)]
+    y = [[] for _ in range(NUM_USERS)]
+    idx = np.zeros(num_class, dtype=np.int64)
+    for user in range(NUM_USERS):
+        for j in range(NUM_LABELS):  # 3 labels for each users
+            #l = (2*user+j)%10
+            l = (user + j) % 10
+            print("L:", l)
+            X[user] += cifa_data[l][idx[l]:idx[l]+10].tolist()
+            y[user] += (l*np.ones(10)).tolist()
+            idx[l] += 10
+
+    print("IDX1:", idx)  # counting samples for each labels
+
+    # Assign remaining sample by power law
+    user = 0
+    props = np.random.lognormal(
+        0, 2., (10, NUM_USERS, NUM_LABELS))  # last 5 is 5 labels
+    props = np.array([[[len(v)-NUM_USERS]] for v in cifa_data]) * \
+        props/np.sum(props, (1, 2), keepdims=True)
+    # print("here:",props/np.sum(props,(1,2), keepdims=True))
+    #props = np.array([[[len(v)-100]] for v in mnist_data]) * \
+    #    props/np.sum(props, (1, 2), keepdims=True)
+    #idx = 1000*np.ones(10, dtype=np.int64)
+    # print("here2:",props)
+    for user in trange(NUM_USERS):
+        for j in range(NUM_LABELS):  # 4 labels for each users
+            # l = (2*user+j)%10
+            l = (user + j) % 10
+            num_samples = int(props[l, user//int(NUM_USERS/10), j])
+            numran1 = random.randint(300, 600)
+            num_samples = (num_samples)  + numran1 #+ 200
+            if(NUM_USERS <= 20): 
+                num_samples = num_samples * 2
+            if idx[l] + num_samples < len(cifa_data[l]):
+                X[user] += cifa_data[l][idx[l]:idx[l]+num_samples].tolist()
+                y[user] += (l*np.ones(num_samples)).tolist()
+                idx[l] += num_samples
+                print("check len os user:", user, j,
+                    "len data", len(X[user]), num_samples)
+
+    print("IDX2:", idx) # counting samples for each labels
+
+    # Create data structure
+    train_data = {'users': [], 'user_data':{}, 'num_samples':[]}
+    test_data = {'users': [], 'user_data':{}, 'num_samples':[]}
+
+    # Setup 5 users
+    # for i in trange(5, ncols=120):
+    for i in range(NUM_USERS):
+        uname = i
+        combined = list(zip(X[i], y[i]))
+        random.shuffle(combined)
+        X[i][:], y[i][:] = zip(*combined)
+
+        num_samples = len(X[i])
+        train_len = int(0.75*num_samples)
+        test_len = num_samples - train_len
+
+        #X_train, X_test, y_train, y_test = train_test_split(X[i], y[i], train_size=0.75, stratify=y[i])\
+        
+        test_data['users'].append(uname)
+        test_data["user_data"][uname] =  {'x': X[i][:test_len], 'y': y[i][:test_len]} 
+        test_data['num_samples'].append(test_len)
+
+        train_data["user_data"][uname] =  {'x': X[i][test_len:], 'y': y[i][test_len:]}
+        train_data['users'].append(uname)
+        train_data['num_samples'].append(train_len)
+         
+
+    return train_data['users'], _ , train_data['user_data'], test_data['user_data']
